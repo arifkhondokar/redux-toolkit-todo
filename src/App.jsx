22adcfc28c41c3dad3649d0872a42e9c3884f92b
arifkhondokar <arifkhondokar1993@gmail.com>
2024-01-04 17:01:57 +0600

    react redux toolkit todo app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+// function App() {
+//   return (
+//     <>
+//     </>
+//   )
+// }
+// export default App
+
+
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 >Redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todos.jsx
@@ -0,0 +1,19 @@
+import "./todos.css"
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+function Todos() {
+    const todos = useSelector((state) => state.todos)
+    const dispatch = useDispatch();
+    return (
+        <>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.text}<button onClick={() => dispatch(removeTodo(todo.id))}>Delete</button></li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Todos
